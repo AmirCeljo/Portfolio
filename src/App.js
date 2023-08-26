90436f1649ff362ca57209fb3141f1b7fdc11cba
Amir Celjo <amir.celjo2@gmail.com>
2023-08-26 19:14:46 +0200

    brrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route, BrowserRouter} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Portfolio from './portfolio/Portfolio';
 import Work from './work/Work';
 function App() {
   return (
     <>
-    <HashRouter>
+    <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route path='Portfolio/' element={<Portfolio/>}/>
         <Route path='/work' element={<Work/>}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   );
 }
