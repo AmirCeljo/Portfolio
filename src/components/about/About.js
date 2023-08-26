f06a775266ee7d0c48e246102dc8ec6e4569b262
Amir Celjo <amir.celjo2@gmail.com>
2023-08-26 18:46:32 +0200

    new commit

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -21,8 +21,8 @@ const About = () => {
           <div>
             <h1>{item.heading} <br/> {item.heading2}</h1>
             <p>{item.p}</p>
-            {index === 0 ?<button onClick={() => navigate('/work')}>See my work <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button> :
-            <button onClick={() => navigate('/write')}>Read my articles <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button>
+            {index === 0 ?<button onClick={() => navigate('Portfolio/work')}>See my work <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button> :
+            <button onClick={() => navigate('Portfolio/write')}>Read my articles <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button>
             }
            </div>
         </div>))}
