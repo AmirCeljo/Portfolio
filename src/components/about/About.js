c18c0ed9a216455b35be799cce01271a32d6e883
Amir Celjo <amir.celjo2@gmail.com>
2023-08-26 18:43:28 +0200

    route

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './about.css'
 import { Link } from 'react-router-dom'
 
-
+import { useNavigate } from 'react-router-dom'
 const About = () => {
 
   const content = [
@@ -11,6 +11,7 @@ const About = () => {
   
   ]
 
+  const navigate = useNavigate();
 
   return (
     <div className='about-content'>
@@ -20,8 +21,8 @@ const About = () => {
           <div>
             <h1>{item.heading} <br/> {item.heading2}</h1>
             <p>{item.p}</p>
-            {index === 0 ?<Link to='/work'>See my work <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></Link> :
-            <Link to='/blog'>Read my articles <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></Link>
+            {index === 0 ?<button onClick={() => navigate('/work')}>See my work <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button> :
+            <button onClick={() => navigate('/write')}>Read my articles <svg width="72" height="22" viewBox="0 0 72 22" xmlns="http://www.w3.org/2000/svg" class="bow-arrow"><path fill="none" stroke="#4831d4" stroke-width="2" stroke-miterlimit="0" d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"></path></svg></button>
             }
            </div>
         </div>))}
