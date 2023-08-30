9a9f55e133b7483933d92b4e6fd88225454edfb6
Amir Celjo <amir.celjo2@gmail.com>
2023-08-30 16:17:51 +0200

    added picture

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './experience.css'
+const Experience = () => {
+  return (
+    <div className='experience'>
+        <div className="experience-content"></div>
+        <div className="experience-thumb"></div>
+    </div>
+  )
+}
+
+export default Experience
--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -3,12 +3,14 @@ import './portfolio.css'
 import MainPage from '../components/main/MainPage'
 import About from '../components/about/About'
 import Brief from '../components/brief/Brief'
+import Experience from '../components/experience/Experience'
 const Portfolio = () => {
   return (
     <>
       <MainPage/>
       <About/>
       <Brief/>
+      <Experience/>
     </>
   )
 }
